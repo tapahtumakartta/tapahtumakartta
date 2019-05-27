d05282d4353bcc019d5ada2e6de20d69db1a217c
Sami Jakonen <44066308+jakonen@users.noreply.github.com>
2019-05-28 01:27:32 +0300

    Start over and switch to Flask

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 # Import modules
-import http.server
-import socketserver
-
-# Run an arbitrary port to which the server points
-# using an apache2 proxy
-PORT = 6000
-
-# Setup a server handler
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    # Start the server
-    print("Listening on port", PORT)
-    httpd.serve_forever()
+import flask
