cb2b2ea51520e879940a60473a2edc7ad710b245
Sami Jakonen <moro@vey.cool>
2019-06-02 21:59:07 +0300

    Create a skeleton for the install script

---
+++ INSTALL.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# TAPAHTUMAKARTTA
+# https://github.com/tapahtumakartta/tapahtumakartta-interface
+# 
+# Licensed under the
+# GNU General Public License v3.0
+# 
+# Description:
+#   Install script for deploying the program
+
+echo "Welcome"
